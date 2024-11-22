b9153c022f8a03dcd3166c5345ee9b181c918e38
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2024-11-22 14:05:31 -0300

    feat: separate components and create filters

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,16 +1,14 @@
-import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
-import { Button } from '../../../components/ui/button'
-import { Input } from '../../../components/ui/input'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '../../../components/ui/table'
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
 
 export const Orders = () => {
   return (
@@ -19,10 +17,7 @@ export const Orders = () => {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
         <div className="space-y-2.5">
-          <form action="" className="flex items-center gap-2">
-            <span className="text-sm font-semibold">Filtros:</span>
-            <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-          </form>
+          <OrderTableFilters />
 
           <div className="rounded-md border">
             <Table>
@@ -40,44 +35,7 @@ export const Orders = () => {
               </TableHeader>
               <TableBody>
                 {Array.from({ length: 20 }).map((_, i) => (
-                  <TableRow key={i}>
-                    <TableCell>
-                      <Button variant="outline" size={'xs'}>
-                        <Search className="h-3 w-3" />
-                        <span className="sr-only">Detalhes do pedido</span>
-                      </Button>
-                    </TableCell>
-                    <TableCell className="font-mono text-xs font-medium">
-                      465465ffss8999
-                    </TableCell>
-                    <TableCell className="text-muted-foreground">
-                      há 15 minutos
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell className="font-medium">
-                      Wesley Fracanábia dos santos
-                    </TableCell>
-                    <TableCell className="font-medium">R$ 149,00</TableCell>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <ArrowRight className="mr-2 h-3 w-3" />
-                        Aprovar
-                      </Button>
-                    </TableCell>
-                    <TableCell>
-                      <Button variant="ghost" size="xs">
-                        <X className="mr-2 h-3 w-3" />
-                        Cancelar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
+                  <OrderTableRow key={i} />
                 ))}
               </TableBody>
             </Table>
